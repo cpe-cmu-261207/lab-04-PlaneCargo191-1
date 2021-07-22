f2d797f6a39ca0e69a5466d5388f821a08479040
PlaneCargo191 <t.james_17@hotmail.com>
2021-07-22 16:08:45 +0700

    Task List added

--- script.js
+++ script.js
@@ -3,13 +3,15 @@ const input = document.getElementById('input')
 const addBtn = document.getElementById('addBtn')
 const main = document.createElement('div')
 const spanTask = document.createElement('span')
+const todoList = document.createElement('ul')
 
 //Add List by Clicking
 addBtn.addEventListener('click', () => {
-    const span = document.createElement('span')
-    span.innerHTML = input.value
-    spanTask.append(span)
-    spanTask.append(document.createElement('br'))
+    const newItem = document.createElement("li")
+    let item = input.value
+    newItem.append(item)
+    todoList.insertBefore(newItem, todoList.childNodes[0])
+    spanTask.append(todoList)
     main.append(spanTask)
 })
 
@@ -17,10 +19,11 @@ addBtn.addEventListener('click', () => {
 input.addEventListener('keypress', ev => {
     if(ev.code === 'Enter') {
         ev.preventDefault()
-        const span = document.createElement('span')
-        span.innerHTML = input.value
-        spanTask.append(span)
-        spanTask.append(document.createElement('br'))
+        const newItem = document.createElement("li")
+        let item = input.value
+        newItem.append(item)
+        todoList.insertBefore(newItem, todoList.childNodes[0])
+        spanTask.append(todoList)
         main.append(spanTask)
     }
 })
