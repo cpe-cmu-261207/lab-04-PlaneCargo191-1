b7102d5655c99c67a0556eb3611870dd390b3c06
PlaneCargo191 <t.james_17@hotmail.com>
2021-07-22 15:20:30 +0700

    ADD Button Code added

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-/* Your code here */
+//Declare
+const input = document.getElementById('input')
+const addBtn = document.getElementById('addBtn')
+const main = document.createElement('div')
+
+//Add List
+addBtn.addEventListener('click', () => {
+    const spanTask = document.createElement('span')
+    const span = document.createElement('span')
+    span.innerHTML = input.value
+    spanTask.append(span)
+    spanTask.append(document.createElement('br'))
+    main.append(spanTask)
+})
+
+document.body.append(main)
