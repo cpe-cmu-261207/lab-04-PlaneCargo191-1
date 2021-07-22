0513d796d35da3b0ea9cf3c0434fb4bebefcfdf3
PlaneCargo191 <t.james_17@hotmail.com>
2021-07-22 22:55:48 +0700

    DONE & REMOVE Button Code added

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+//import CSS
+var head = document.getElementsByTagName('HEAD')[0]
+var link = document.createElement('link')
+link.rel = 'stylesheet'
+link.type = 'text/css'
+link.href = 'style.css'
+head.appendChild(link)
+
 //Declare
 const input = document.getElementById('input')
 const addBtn = document.getElementById('addBtn')
@@ -8,9 +16,40 @@ const todoList = document.createElement('ul')
 //Add List by Clicking
 addBtn.addEventListener('click', () => {
     if(input.value.length != 0) {
+        //Declare
         const newItem = document.createElement("li")
+        newItem.style.textDecoration = 'none'
+        const doneBtn = document.createElement('button')
+        const delBtn = document.createElement('button')
+
+        doneBtn.innerHTML = 'DONE'
+        doneBtn.className = 'doneBtn'
+        delBtn.innerHTML = 'DELETE'
+        delBtn.className = 'delBtn'
+
+        //Function
+        newItem.addEventListener('mouseover', () => {
+            doneBtn.style.visibility = 'visible'
+            delBtn.style.visibility = 'visible'
+        })
+        newItem.addEventListener('mouseleave', () => {
+            doneBtn.style.visibility = 'hidden'
+            delBtn.style.visibility = 'hidden'
+        })
+        doneBtn.addEventListener('click', () => {
+            newItem.removeChild(doneBtn)
+            newItem.removeChild(delBtn)
+            newItem.style.textDecoration = 'line-through'
+        })
+        delBtn.addEventListener('click', () => {
+            todoList.removeChild(newItem)
+        })
+
+        //Get Input
         let item = input.value
         newItem.append(item)
+        newItem.append(doneBtn)
+        newItem.append(delBtn)
         todoList.insertBefore(newItem, todoList.childNodes[0])
         spanTask.append(todoList)
         main.append(spanTask)
