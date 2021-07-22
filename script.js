d5782fe3745c6b16b9fd9e0d3f5bae2c735bd649
PlaneCargo191 <t.james_17@hotmail.com>
2021-07-22 15:33:59 +0700

    ADD Button Code by Enter Key added

--- script.js
+++ script.js
@@ -2,15 +2,27 @@
 const input = document.getElementById('input')
 const addBtn = document.getElementById('addBtn')
 const main = document.createElement('div')
+const spanTask = document.createElement('span')
 
-//Add List
+//Add List by Clicking
 addBtn.addEventListener('click', () => {
-    const spanTask = document.createElement('span')
     const span = document.createElement('span')
     span.innerHTML = input.value
     spanTask.append(span)
     spanTask.append(document.createElement('br'))
     main.append(spanTask)
+})
+
+//Add List by Enter Key
+input.addEventListener('keypress', ev => {
+    if(ev.code === 'Enter') {
+        ev.preventDefault()
+        const span = document.createElement('span')
+        span.innerHTML = input.value
+        spanTask.append(span)
+        spanTask.append(document.createElement('br'))
+        main.append(spanTask)
+    }
 })
 
 document.body.append(main)
