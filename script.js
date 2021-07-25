042a0031fe4e0668bf72df1126bcde6ceb63cfcc
PlaneCargo191 <t.james_17@hotmail.com>
2021-07-25 22:53:05 +0700

    localStorage unfinished

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//import CSS
+//Import CSS
 var head = document.getElementsByTagName('head')[0]
 var link = document.createElement('link')
 link.rel = 'stylesheet'
@@ -13,6 +13,10 @@ const main = document.createElement('div')
 const tasks = document.createElement('div')
 const todoList = document.createElement('div')
 const doneList = document.createElement('div')
+let todo = []
+let done = []
+
+main.innerHTML = localStorage.getItem('l')
 
 todoList.style.textAlign = 'left'
 doneList.style.textAlign = 'right'
@@ -20,21 +24,24 @@ doneList.style.marginRight = '40px'
 tasks.style.display = 'flex'
 tasks.style.justifyContent = 'space-between'
 
-const doList = () => {
+const doList = n => {
     if(input.value.length != 0) {
         //Declare
         const newItem = document.createElement('span')
         const doneBtn = document.createElement('button')
         const delBtn = document.createElement('button')
-        const btn = document.createElement('button')
+        const btn = document.createElement('div')
         const list = document.createElement('div')
 
         doneBtn.innerHTML = 'DONE'
         doneBtn.className = 'doneBtn'
         delBtn.innerHTML = 'DELETE'
         delBtn.className = 'delBtn'
-
         newItem.className = 'item'
+
+        todo.push(n)
+        localStorage.setItem('todo', todo)
+        localStorage.setItem('l', main.innerHTML)
 
         //Function
         list.addEventListener('mouseover', () => {
@@ -49,23 +56,33 @@ const doList = () => {
             list.removeChild(btn)
             doneList.insertBefore(list, doneList.childNodes[0])
             newItem.className = 'doneItem'
+            done.push(newItem.innerHTML)
+            todo.splice(todo.findIndex(x => (x === newItem.innerHTML)), 1)
+            localStorage.setItem('todo', todo)
+            localStorage.setItem('done', done)
+            localStorage.setItem('l', main.innerHTML)
         })
         delBtn.addEventListener('click', () => {
             todoList.removeChild(list)
+            todo.splice(todo.findIndex(x => (x === newItem.innerHTML)), 1)
+            localStorage.setItem('todo', todo)
+            localStorage.setItem('l', main.innerHTML)
         })
 
         //Get Input
-        let item = input.value
-        newItem.append(item)
+        let item = n
+        newItem.innerHTML = item
         btn.append(doneBtn)
         btn.append(delBtn)
         list.append(newItem)
         list.append(btn)
+        list.style.display = 'flex'
         list.append(document.createElement('br'))
         todoList.insertBefore(list, todoList.childNodes[0])
         tasks.append(todoList)
         tasks.append(doneList)
         main.append(tasks)
+        input.value = ''
     } else {
         alert('The Task Box is NOT Filled')
     }
@@ -73,14 +90,15 @@ const doList = () => {
 
 //Add List by Clicking
 addBtn.addEventListener('click', () => {
-    doList()
+    doList(input.value)
+    console.log(typeof(input.value))
 })
 
 //Add List by Enter Key
 input.addEventListener('keypress', ev => {
     if(ev.code === 'Enter') {
         ev.preventDefault()
-        doList()
+        doList(input.value)
     }
 })
 
